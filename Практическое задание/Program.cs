bd97283819f9533653f2c72d9763e8d9ec25bf2a
Sergey <manerov.s@nail.ru>
2022-12-23 16:45:15 +0300

    попытка сделать методы

--- Практическое задание/Program.cs
+++ Практическое задание/Program.cs
@@ -1,60 +1,77 @@
-﻿string[] color = { "Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый" };
-
-Console.WriteLine("Вам предоставлено семь цветов: ");
-
-for (int i = 0; i < color.Length; i++)
+﻿void Info(string[] color)
 {
-    if (i < color.Length - 1)
-        Console.Write($"{i + 1}. {color[i]}, ");
-    else
-        Console.WriteLine($"{i + 1}. {color[i]}");
+    Console.WriteLine("Вам предоставлено семь цветов: ");
+
+    for (int i = 0; i < color.Length; i++)
+    {
+        if (i < color.Length - 1)
+            Console.Write($"{i + 1}. {color[i]}, ");
+        else
+            Console.WriteLine($"{i + 1}. {color[i]}");
+    }
 }
 
-string count;
-Console.WriteLine("Введите числом сколько цветов из представленных вам нравятся: ");
-count = Console.ReadLine();
-if (string.IsNullOrEmpty(count))
+int InputData1(string[] color)
 {
-    Console.WriteLine("Вам не нравятся ни один из предоставленных цветов :( ");
-    return;
-}
-int numb;
-numb = Convert.ToInt32(count);
-if (numb == 0)
+    string count;
+    Console.WriteLine("Введите числом сколько цветов из представленных вам нравятся: ");
+    count = Console.ReadLine();
+    if (string.IsNullOrEmpty(count))
         Console.WriteLine("Вам не нравятся ни один из предоставленных цветов :( ");
-else
+    else return count;
+}
+
+int InputData2(string[] color)
 {
-    while (numb <= 0 || numb > 7)
+    int numb;
+    numb = Convert.ToInt32(count);
+    if (numb == 0)
+        Console.WriteLine("Вам не нравятся ни один из предоставленных цветов :( ");
+    else
     {
-        Console.WriteLine("Введено число не соответствующее количеству предоставленных цветов." );
-        Console.WriteLine("Введите числом сколько цветов из представленных вам нравятся: ");
-        numb = Convert.ToInt32(Console.ReadLine());
-    }
-
-    if (numb > 0 && numb <= 7)
-    {
-        string[] userColor = new string[numb];
-        Console.WriteLine("Через ENTER укажите числа соответствующие данным цветам");
-        int like;
-        for (int j = 0; j < userColor.Length; j++)
+        while (numb <= 0 || numb > 7)
         {
-            like = Convert.ToInt32(Console.ReadLine());
-            while (like <= 0 || like > 7)
+            Console.WriteLine("Введено число не соответствующее количеству предоставленных цветов." );
+            Console.WriteLine("Введите числом сколько цветов из представленных вам нравятся: ");
+            numb = Convert.ToInt32(Console.ReadLine());
+        }
+
+        if (numb > 0 && numb <= 7)
+        {
+            string[] userColor = new string[numb];
+            Console.WriteLine("Через ENTER укажите числа соответствующие данным цветам");
+            int like;
+            for (int j = 0; j < userColor.Length; j++)
             {
-                Console.WriteLine("Введено число не соответствующее предоставленному цвету. Укажите числа соответствующие данным цветам: ");
                 like = Convert.ToInt32(Console.ReadLine());
+                while (like <= 0 || like > 7)
+                {
+                    Console.WriteLine("Введено число не соответствующее предоставленному цвету. Укажите числа соответствующие данным цветам: ");
+                    like = Convert.ToInt32(Console.ReadLine());
+                }
+                userColor[j] = color[like - 1];
             }
-            userColor[j] = color[like - 1];
+            return userColor;
         }
+    }   
+}
 
-        Console.WriteLine("Выбранные вами цвета: ");
-        for (int k = 0; k < userColor.Length; k++)
-        {
-            if (k < userColor.Length - 1)
-                Console.Write($"{userColor[k]}, ");
-            else
-                Console.WriteLine(userColor[k]);
-        }
+
+
+void UserColor(string[] userColor)
+{
+    Console.WriteLine("Выбранные вами цвета: ");
+    for (int k = 0; k < userColor.Length; k++)
+    {
+        if (k < userColor.Length - 1)
+            Console.Write($"{userColor[k]}, ");
+        else
+            Console.WriteLine(userColor[k]);
     }
 }
 
+
+string[] color = { "Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый" };
+Info(color);
+InputData(color);
+UserColor(userColor);
