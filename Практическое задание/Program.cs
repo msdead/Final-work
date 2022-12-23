9baeb6e40c97244973d72101aa894d182e130f57
Sergey <manerov.s@nail.ru>
2022-12-23 14:45:55 +0300

    готовая основа задания

--- Практическое задание/Program.cs
+++ Практическое задание/Program.cs
@@ -1,6 +1,42 @@
-﻿string[] color = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый"};
-Console.WriteLine("Выберите какие цвета из представленных вам нравятся: ");
-for (int i = 0; i < length; i++)
+﻿string[] color =
 {
-    Console.WriteLine(color[i]);
+    "1. Красный",
+    "2. Оранжевый",
+    "3. Желтый",
+    "4. Зеленый",
+    "5. Голубой",
+    "6. Синий",
+    "7. Фиолетовый"
+};
+
+Console.WriteLine("Вам предоставлено семь цветов: ");
+
+for (int i = 0; i < color.Length; i++)
+{
+    if (i < color.Length - 1)
+        Console.Write($"{color[i]}, ");
+    else
+        Console.WriteLine(color[i]);
+}
+
+
+int count;
+Console.WriteLine("Укажите числом сколько цветов из представленных вам нравятся");
+count = Convert.ToInt32(Console.ReadLine());
+string[] userColor = new string[count];
+Console.WriteLine("Через ENTER укажите цифры соответствующие данным цветам");
+int like;
+for (int j = 0; j < userColor.Length; j++)
+{
+    like = Convert.ToInt32(Console.ReadLine());
+    userColor[j] = color[like-1];
+}
+
+Console.WriteLine("Выбранные вами цвета: ");
+for (int k = 0; k < userColor.Length; k++)
+{
+    if (k < userColor.Length - 1)
+        Console.Write($"{userColor[k]}, ");
+    else
+        Console.WriteLine(userColor[k]);
 }
