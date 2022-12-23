786d3fd1e8557753d44cb13cf5a764cf2608b57b
Sergey <manerov.s@nail.ru>
2022-12-23 12:16:11 +0300

    Заготовка

--- Практическое задание/Program.cs
+++ Практическое задание/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] color = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый"};
+Console.WriteLine("Выберите какие цвета из представленных вам нравятся: ");
+Console.WriteLine(color[]);
