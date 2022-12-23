1d411e87fb93a0a9f349ce8a9dae9013be24e2a9
Sergey <manerov.s@nail.ru>
2022-12-23 12:18:11 +0300

    Заготовка

--- Практическое задание/Program.cs
+++ Практическое задание/Program.cs
@@ -1,3 +1,6 @@
 ﻿string[] color = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый"};
 Console.WriteLine("Выберите какие цвета из представленных вам нравятся: ");
-Console.WriteLine(color[]);
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine(color[i]);
+}
